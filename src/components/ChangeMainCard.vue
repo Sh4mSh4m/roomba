ad4c5820c42af66cdccd99a1faf218b107d9997e
hieu@mb12 <hieu2d@gmail.com>
2020-01-02 18:19:19 +0100

    updated draft 101 with new components

---
+++ src/components/ChangeMainCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <v-card-title>Change#id</v-card-title>
+    <v-card-subtitle>
+      <ul>
+        <li>status: clear</li>
+        <li>qual env status: ready</li>
+      </ul>
+    </v-card-subtitle>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+
+      <BtnTooltipBottom icon="mdi-forklift" tooltiptext="Prepare qualification environment" />
+
+      <BtnTooltipBottom
+        icon="mdi-checkbox-multiple-marked-circle-outline"
+        tooltiptext="Check running conf in qual env"
+      />
+
+      <BtnTooltipBottom icon="mdi-heart" tooltiptext="test" />
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import BtnTooltipBottom from "@/components/buttons/ButtonTooltipBottom.vue";
+
+export default {
+  components: {
+    BtnTooltipBottom
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/buttons/ButtonTooltipBottom.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on }">
+      <v-btn v-on="on" color="primary" icon>
+        <v-icon>{{ icon }}</v-icon>
+      </v-btn>
+    </template>
+    <span>{{ tooltiptext }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: String,
+    tooltiptext: String
+  }
+};
+</script>
+
+<style>
+</style>
