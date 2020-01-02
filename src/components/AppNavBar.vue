7e5520471b709e834c8f30dff1a12490cce8cfde
hieu@mb12 <hieu2d@gmail.com>
2020-01-02 16:42:36 +0100

    change show view

---
+++ src/components/AppNavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-app-bar color="green accent-4" app>
+      <v-toolbar-title>Roomba</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn :to="{name: 'home'}" text rounded>
+        Home <v-icon>mdi-home-circle-outline</v-icon>
+      </v-btn>
+
+      <v-btn :to="{name: 'change', params: { changeid: '1' }}" text rounded>
+        Change#1
+        <v-icon>mdi-file-move-outline</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <!-- Future nav list
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+        -->
+      </v-menu>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/ChangeShow.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>This is the change page for change #{{ changeid }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['changeid']
+};
+</script>
+
+<style>
+</style>
