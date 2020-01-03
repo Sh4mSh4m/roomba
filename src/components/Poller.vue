f850413b2895e6420c376d1f180790969314a7ed
Sh4M <hieu2d@gmail.com>
2020-01-04 00:33:50 +0100

    poller added

---
+++ src/components/Poller.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+//webs.onmessage = function(event) {
+//  console.log(event.data);
+//};
+export default {
+  data() {
+    return {
+      message: {
+        action: "register",
+        change_id: "1",
+        source: "client"
+      }
+    };
+  },
+  methods: {
+    connect() {
+      this.socket = new WebSocket("ws://localhost:8765");
+      this.socket.onopen = () => this.socket.send(JSON.stringify(this.message));
+      this.socket.onmessage = event => console.log(event.data);
+    }
+  },
+  created() {
+    this.connect();
+  }
+};
+</script>
+
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Carousel />
+    <Poller />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Carousel from "@/components/Carousel.vue"
+import Carousel from "@/components/Carousel.vue";
+import Poller from "@/components/Poller.vue";
 
 export default {
   name: "home",
   components: {
-    Carousel
+    Carousel,
+    Poller
   }
 };
 </script>
